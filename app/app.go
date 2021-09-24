0e62f4f7ea6dc8f3c4bf21b2b964d3c5e0f7edb8
Damian Nolan <damiannolan@gmail.com>
2021-09-24 11:06:34 +0200

    adding ica module account to app.go

--- app/app.go
+++ app/app.go
@@ -156,6 +156,7 @@ var (
 		stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
 		govtypes.ModuleName:            {authtypes.Burner},
 		ibctransfertypes.ModuleName:    {authtypes.Minter, authtypes.Burner},
+		icatypes.ModuleName:            nil,
 	}
 
 	// module accounts that are allowed to receive tokens
