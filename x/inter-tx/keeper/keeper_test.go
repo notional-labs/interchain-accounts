adeca228c8c54144dc599a333affc22f48376e29
Damian Nolan <damiannolan@gmail.com>
2021-12-02 16:51:02 +0100

    adding keeper test suite boilerplate setup (#67)

---
+++ x/inter-tx/keeper/keeper_test.go
@@ -0,0 +1,48 @@
+package keeper_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+
+	ibctesting "github.com/cosmos/ibc-go/v2/testing"
+
+	icaapp "github.com/cosmos/interchain-accounts/app"
+)
+
+func init() {
+	ibctesting.DefaultTestingAppInit = SetupICATestingApp
+}
+
+func SetupICATestingApp() (ibctesting.TestingApp, map[string]json.RawMessage) {
+	db := dbm.NewMemDB()
+	encCdc := icaapp.MakeEncodingConfig()
+	app := icaapp.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, icaapp.DefaultNodeHome, 5, encCdc, icaapp.EmptyAppOptions{})
+	return app, icaapp.NewDefaultGenesisState(encCdc.Marshaler)
+}
+
+// KeeperTestSuite is a testing suite to test keeper functions
+type KeeperTestSuite struct {
+	suite.Suite
+
+	coordinator *ibctesting.Coordinator
+
+	// testing chains used for convenience and readability
+	chainA *ibctesting.TestChain
+	chainB *ibctesting.TestChain
+}
+
+// TestKeeperTestSuite runs all the tests within this package.
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
+
+// SetupTest creates a coordinator with 2 test chains.
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 2)
+	suite.chainA = suite.coordinator.GetChain(ibctesting.GetChainID(0))
+	suite.chainB = suite.coordinator.GetChain(ibctesting.GetChainID(1))
+}
