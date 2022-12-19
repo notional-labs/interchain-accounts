f3037dc7defbe1bc2be5021cd42f5598e1550e23
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-19 18:31:07 +0700

    extra linted

--- x/inter-tx/ibc_module.go
+++ x/inter-tx/ibc_module.go
@@ -149,7 +149,7 @@ func (im IBCModule) OnTimeoutPacket(
 	return nil
 }
 
-func handleMsgData(ctx sdk.Context, msgData *sdk.MsgData) (string, error) {
+func handleMsgData(ctx sdk.Context, msgData *sdk.MsgData) (string, error) { //nolint:staticcheck
 	switch msgData.MsgType {
 	case sdk.MsgTypeURL(&banktypes.MsgSend{}):
 		msgResponse := &banktypes.MsgSendResponse{}
