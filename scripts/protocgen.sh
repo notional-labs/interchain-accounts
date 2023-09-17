90292352193c1d90454652dde9e850524f3a076d
Jacob Gadikian <jacobgadikian@gmail.com>
2023-09-17 11:06:21 +0800

    kick off the contract executor thing

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,17 +2,15 @@
 
 set -eo pipefail
 
-# Get the path of the cosmos-sdk repo from go/pkg/mod
-# cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  buf alpha protoc \
-    -I "proto" \
-    -I "third_party/proto" \
-    --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-    --grpc-gateway_out=logtostderr=true:. \
-    $(find "${dir}" -maxdepth 1 -name '*.proto')
-done
+echo "Generating gogo proto code"
+cd proto
 
-cp -r github.com/cosmos/interchain-accounts/x/* x/
+buf generate --template buf.gen.gogo.yaml $file
+
+cd ..
+
+# move proto files to the right places
+cp -r github.com/cosmos/interchain-accounts/v*/x/* x/
 rm -rf github.com
+
+go mod tidy
